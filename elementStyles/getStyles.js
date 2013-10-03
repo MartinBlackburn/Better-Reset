93a9eececc92ff3b638b08aa7760654ab12c4d6f
Martin Blackburn <walking_fishy@hotmail.com>
2013-10-03 15:35:15 +0100

    js now creates a table of all css properties for every element on the page

--- elementStyles/getStyles.js
+++ elementStyles/getStyles.js
@@ -31,7 +31,40 @@ StyleList = function()
         };
     })(jQuery.fn.allcss);
     
-    console.log($(".test").first().allcss());
+    $("*").each(function()
+    {        
+        //get element name
+        var name = $(this).get(0).tagName;
+        
+        //add type if it exists (for inputs)
+        if($(this).get(0).type) {
+            name = name + " - " + $(this).get(0).type;
+        }
+        
+        //add nesting level for LIs
+        if($(this).get(0).tagName == "LI") {
+            name = name + " (depth  " + $(this).parents("ol, ul").length + ")";
+        }
+        
+        //all css
+        var styles = $(this).allcss();        
+        createTable(name, styles);
+    });
+    
+    //format all styles into a table.
+    function createTable(name, styles)
+    {
+        //table heading with name
+        var table = $("<table></table>");
+        table.append('<thead><tr><th colspan="2">' + name + '</th></tr></thead>');
+        
+        //rows for each style
+        $.each(styles, function(key, value) {
+            table.append('<tr><td>' + key + '</td><td>' + value + '</td></tr>');
+        });
+        
+        $('body').append(table);
+    }
 }
 
 $(function() 
