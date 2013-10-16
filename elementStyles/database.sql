e37f9eab4cb85a478ec1f955cf62dceb18b2d152
Martin Blackburn <walking_fishy@hotmail.com>
2013-10-16 16:10:52 +0100

    added sql file for database

---
+++ elementStyles/database.sql
@@ -0,0 +1,29 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+CREATE TABLE `Browsers` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Version` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `Elements` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `Properties` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Display` int(11) NOT NULL DEFAULT '1',
+  `Value` varchar(255) NOT NULL,
+  `ElementID` int(11) NOT NULL,
+  `BroswerID` int(11) NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
+SET FOREIGN_KEY_CHECKS = 1;
