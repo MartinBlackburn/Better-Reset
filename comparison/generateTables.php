8a8a64262eafedb207f53a7b5c83fdf1b93f27fd
Martin Blackburn <walking_fishy@hotmail.com>
2013-10-18 16:12:13 +0100

    removed tempalte/content elements, started loops for generating tables

--- comparison/generateTables.php
+++ comparison/generateTables.php
@@ -12,5 +12,12 @@ $elements = $databaseModel->getElements();
 
 //foreach element, create a table, with a column for each browser
 foreach($elements as $element) {
+    $elementID = $element['ID'];
+    $elementName = $element['Name'];
+    $elementType = $element['Type'];
     
+    foreach($browsers as $browser) {
+        $browserID = $browser['ID'];
+        $browserName = $browser['Name'];
+    }
 }
